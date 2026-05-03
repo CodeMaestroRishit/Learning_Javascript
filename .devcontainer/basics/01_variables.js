8c08b7012c8b054c53e1f9ec5646d2a767d77548
rishit_0801 <154057402+CodeMaestroRishit@users.noreply.github.com>
2026-05-03 06:03:43 +0000

    Basics

---
+++ .devcontainer/basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId=144453
+let accountEmail="rishit@google.com"
+var password="12234"//it is variable and can be changed but cant use scope
+accountCity="Jaipur"
+
+//accountId=3
+console.log(accountId);
+accountEmail="hchc@gmail.com"
+accPasword="21221"
+accountCity="BLR"
+let accountcity;// let is a variavle but you can use scope: which is { used in if else}
+
+console.table([accountId,accountEmail,accountCity,accPasword,accountcity]); //printing all together
---
+++ .devcontainer/basics/02_dataTypes.js
@@ -0,0 +1,3 @@
+"use strict" //treat all js code as newer version
+//alert(3+3) (can be used in browser to see solution but this is Node JS)
+console.log(3+3);
