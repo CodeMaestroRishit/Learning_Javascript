556079f87810a71150482998562d65d1800e830b
rishit_0801 <154057402+CodeMaestroRishit@users.noreply.github.com>
2026-05-03 06:26:09 +0000

    did operations

---
+++ basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict" //treat all js code as newer version
+//alert(3+3) (can be used in browser to see solution but this is Node JS)
+console.log(3+3);
+
+let age=18
+/*
+number=>2
+bigint
+string=""
+boolean=true/false
+null=standalone value
+undefined=>
+    symbol->unique
+
+typeofnull=>object
+typeofundefined=>undefined
+
+*/
+console.log(typeof age);
---
+++ basics/03_conversionOperations.js
@@ -0,0 +1,14 @@
+let score="ans"
+console.log(typeof score);
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber)// output is number but its not true as there is 44ans which is combination of number+string
+console.log(valueInNumber);
+//true=1,false=0
+
+let isLoggedIn="";
+let booleanIsLoggedIn=Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn)
+
+//""=>false
+//"rishit"=>true
